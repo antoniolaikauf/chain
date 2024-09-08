079172c1f100a91a1728de60db99c558448b8ac3
antoniolaikauf <antonio.laikauf00@gmail.com>
2024-09-08 16:55:48 +0200

    server

--- DataServer.js
+++
@@ -1,6 +0,0 @@
-
-const axios = require('axios');
-
-axios.get('http://localhost:3000').then((res) => {
-    console.log(res.data);
-})
--- Server.js
+++ Server.js
@@ -1,6 +1,22 @@
-const Http = require('http')
+const net = require('net') // modulo per rete peer to peer 
+const Socket = new net.Socket()
+const event = require('events')
+// const emitter = new EventEmitter()
 
-const server = Http.createServer((rep, res) => {
-    res.write('hello')
-    res.end()
+
+const server = net.createServer((socket) => {
+    socket.on(('data'), (data) => {
+        console.log(data);
+        socket.write('ciao')
+        
+    })
+    socket.on('end', () => {
+        console.log('close');
+    })
+    
 })
+
+server.listen(9000, () => {
+    console.log('server attivo');
+    
+});
