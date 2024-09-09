4040af797fc1ad3b592ba9899467883cb133c282
antoniolaikauf <antonio.laikauf00@gmail.com>
2024-09-09 10:17:15 +0200

    connessione e sconnessione

--- Server.js
+++ Server.js
@@ -3,26 +3,29 @@ const Socket = new net.Socket()
 
 let clients = []
 const server = net.createServer((socket) => {
-    clients.push({'IP':socket.remoteAddress, data:socket})
-    socket.on(('data'), (data) => {
+    
+    clients.push({ 'IP': socket.remoteAddress, data: socket })
+    socket.on('data', (data) => {
         let text =data.toString('utf-8')
         console.log(text);
+        console.log(clients.length);
+        
         clients.forEach((element,i) => {
             console.log(element.IP);
         });
-        socket.write('ciao')
+        socket.write('benvenuto')
     })
-    socket.on('end', () => {
-        console.log('close');
+    socket.on('end', function () {
+        console.log('client disconnesso: ', socket.remoteAddress);
+        clients = clients.filter((element) => element.IP != socket.remoteAddress)
+        console.log(clients);
+        
+    });
+    socket.on('error', (err) => {
+        console.error(`Socket error: ${err.message}`);
     })
 })
 
-server.listen(3000, '0.0.0.0', () => {
-    console.log('server attivo');
-    
+server.listen(3000,'0.0.0.0', () => {
+    console.log('server attivo'); 
 });
-
-server.on('error', (err) => {
-    console.log(err);
-    
-})
--- client.js
+++ client.js
@@ -1,16 +1,29 @@
 const net = require('net')
 const client = new net.Socket()
 
-client.connect(3000, '192.168.1.2', () => {
-    console.log('connessione');
-    client.write('sono client')
+client.connect(3000, '192.168.1.7', () => {
+    console.log('client connesso');
+    client.write('client connesso')
 })
-
+// data ottenuti
 client.on('data', (data) => {
-    console.log(data.toString('utf-8'));
+    const text= data.toString('utf-8')
+    console.log(text);
+})
+// errori
+client.on('error', (err) => {
+    console.error(err.message);
 })
 
-client.on('error', (err) => {
-    console.log(err);
-    
+client.on('end', () => {
+    console.log('connessione chiusa dal server');
+})
+
+process.on('SIGINT', () => {
+    console.log('connessione chiusa');
+    client.end(() => { // chiusura è asincorna quindi messo settimeout 
+        setTimeout(() => {
+            process.exit(0); 
+        }, 100);
+    });
 })
