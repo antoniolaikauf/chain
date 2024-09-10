661a5b33c50ef54dfd268d287390d1216b2ea89f
antoniolaikauf <antonio.laikauf00@gmail.com>
2024-09-10 11:20:34 +0200

    connessione dgram

--- broadcast.js
+++ broadcast.js
@@ -1,4 +1,6 @@
-const dgram = require('dgram');
+
+// UDP è un protocollo di rete che consente l'invio di pacchetti di dati 
+// tra host in una rete senza stabilire una connessione formale 
 const listen_client = new dgram.createSocket('udp4')
 const { Buffer } = require('buffer');
 
@@ -10,21 +12,15 @@ listen_client.on('listening', () => {
     listen_client.setBroadcast(true)
     listen_client.send(message, port, address, (err) => {
         console.log('messaggio inviato');
-        
     })
 })
 
 listen_client.on('error', (err) => {
     console.error(err.message);
-    
 })
 
 listen_client.on('message', (msg, rinfo) => {
     console.log(`messaggio ${msg} da ${rinfo.address} porta ${rinfo.port}`);
-  
 })
-listen_client.bind(port, () => {
-    console.log('connessione');
-})
-
+listen_client.bind(port)
 //192.168.0.255
