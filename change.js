eaebd53a731a1d8060895cc9d5709f185ac6b55b
antoniolaikauf <antonio.laikauf00@gmail.com>
2024-09-11 15:15:36 +0200

    collegamento

--- change.js
+++ change.js
@@ -3,10 +3,9 @@ const listen_server = new dgram.createSocket("udp4");
 const { Buffer } = require("buffer");
 
 const port = 41234;
-const address = '239.255.255.250';
+const address = '255.255.255.255';
 let partecipanti= []
 listen_server.on("listening", () => {
-    listen_server.addMembership(address);
     const message = Buffer.from('collegato'); // string to ascii
     listen_server.setBroadcast(true); // broadcasting messaggio in rete
     
@@ -17,14 +16,13 @@ listen_server.on("error", (err) => {
   console.error(err.message);
 });
 
-listen_server.on("message", (msg, rinfo) => {
-    
+listen_server.on("message", (msg, rinfo) => {   
     if (!partecipanti.includes(rinfo.address)) {
         partecipanti.push(rinfo.address)
-        listen_server.send(Buffer.from(partecipanti),port,address)
+        listen_server.send(Buffer.from(partecipanti), port, address)
+        console.log(partecipanti);
+        
     }   
-    console.log(partecipanti);
-    
     console.log(`${msg} da ${rinfo.address} porta ${rinfo.port}`);
     // server_peer(rinfo.address) // collegamento al computer in rete
 });
