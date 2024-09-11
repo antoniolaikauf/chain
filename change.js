4d3d294268349762b5c803b9391d737105ad3719
antoniolaikauf <antonio.laikauf00@gmail.com>
2024-09-11 18:42:01 +0200

    correzione

--- change.js
+++ change.js
@@ -5,33 +5,26 @@ const { Buffer } = require("buffer");
 const port = 41234;
 const address = '255.255.255.255';
 let partecipanti = []
-let count= 0
-listen_server.on("listening", () => {      
-    // const message = Buffer.from(partecipanti); // string to ascii
-    listen_server.setBroadcast(true); // broadcasting messaggio in rete
-    // listen_server.send(message, port, address); // invio mesaggio
-});
+
+call(partecipanti)
+function call(a) {
+    listen_server.on("listening", () => { 
+        const message = Buffer.from(a); // string to ascii
+        listen_server.setBroadcast(true); // broadcasting messaggio in rete
+        listen_server.send(message, port, address); // invio mesaggio
+    });
+}
 
 listen_server.on("error", (err) => {
   console.error(err.message);
 });
 
 listen_server.on("message", (msg, rinfo) => { 
-    const nuoviPartecipanti = JSON.parse(msg.toString());
-
-    // Check if the address of the sender is already in the list
-    if (!partecipanti.includes(rinfo.address)) {
-        partecipanti.push(rinfo.address); // Add the new peer to the list
-        console.log(`Nuovo peer scoperto: ${rinfo.address}`);
-        console.log(`Lista peer attuale: ${partecipanti}`);
+    let nuoviPartecipanti = Array.from(msg)
+    if (!nuoviPartecipanti.includes(rinfo.address)) {
+        nuoviPartecipanti.push(rinfo.address)
+        call(nuoviPartecipanti)  
     }
-
-    // Convert the updated participants list to a JSON string and then to a Buffer
-    const message = Buffer.from(JSON.stringify(partecipanti));
-    
-    // Send the updated list to the entire network (excluding itself)
-    if (rinfo.address !== listen_server.address().address) {
-        listen_server.send(message, port, address); // Broadcast the updated list
-    }
+    console.log(nuoviPartecipanti);
 });
 listen_server.bind(port);
