c25cfb569eca2735a27120081626da2e1c6f19ad
antoniolaikauf <antonio.laikauf00@gmail.com>
2024-09-11 11:11:36 +0200

    start blockchain

--- blockChain.js
+++ blockChain.js
@@ -20,4 +20,14 @@ class BlockChain {
 
 let Transection = new Transection()
 let block = new Block()
-let chain = new BlockChain()
+let chain = new BlockChain(block)
+
+// nel blocco ci deve essere header con interno precedente hash blocco, target per pow,
+// zii se blocco creato contemporaneamente, altezza, tx_root, nonce, coinbase transection, Timestamp, in blocco tenere solo wallet aggiornato
+// non tutta la copia della blockchain 
+// fuori header tutte le transazioni all'interno del blocco
+
+// transazione, quantità, address, feee, change
+
+// chiave privata e pubblica 
+// creare wallet 
