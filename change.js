db2ccca2310d0ce8352a3863c247525a43e06a5e
antoniolaikauf <antonio.laikauf00@gmail.com>
2024-09-11 17:05:10 +0200

    collegamento

--- change.js
+++ change.js
@@ -4,7 +4,7 @@ const { Buffer } = require("buffer");
 
 const port = 41234;
 const address = '255.255.255.255';
-let partecipanti= new Set()
+let partecipanti = new Set()
 listen_server.on("listening", () => {      
     const message = Buffer.from('collegato'); // string to ascii
     listen_server.setBroadcast(true); // broadcasting messaggio in rete
@@ -15,13 +15,22 @@ listen_server.on("error", (err) => {
   console.error(err.message);
 });
 
-listen_server.on("message", (msg, rinfo) => {   
+listen_server.on("message", (msg, rinfo) => {  
+// TODO MIGLIORARE PERCHè SAREBBE PIù CORRETTTO SE LI FACESSE SUL MSG
+    // const peers = Array.from(msg);
+    // let words =''
+    //     peers.forEach(element => {
+    //         // console.log(element.toString('utf-8'));
+    //        words+= String.fromCharCode(element)  
+    //     });
+    // console.log(words, typeof words);
+    
     listen_server.setBroadcast(true); 
     if (!partecipanti.has(rinfo.address)) {
         partecipanti.add(rinfo.address)
         listen_server.send(Buffer.from(Array.from(partecipanti)), port, address)
         console.log(`Nuovo peer scoperto: ${rinfo.address}`);
-        console.log(`Lista peer attuale: ${partecipanti}`);
+        console.log(`Lista peer attuale: ${JSON.stringify(Array.from(partecipanti))}`);
     }   
 });
 listen_server.bind(port);
