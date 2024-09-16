577b683489fec8ec2dfcfb597d9c5bf926d39396
antoniolaikauf <antonio.laikauf00@gmail.com>
2024-09-16 22:08:51 +0200

    transazione controllo

---
+++ peers/Server.js
@@ -0,0 +1,73 @@
+// nesuna instanza socket con il server perchè il node crea un collegamento socket con il client collegato
+const net = require("net"); // modulo per rete peer to peer
+let blockList = new net.BlockList(); // vedere se mettere regole specifiche tipo se prova a collegarsi più volte da quel ip
+const crypto = require("crypto");
+
+let clients = [];
+const server = net.createServer((socket) => {
+  // controllo e client gia connesso
+  const client = { IP: socket.remoteAddress, data: socket };
+  if (blockList.check(socket.remoteAddress)) {
+    socket.destroy();
+  } else {
+    blockList.addAddress(socket.remoteAddress);
+    clients.push(client);
+  }
+  // data dal client
+  socket.on("data", (data) => {
+      const content = JSON.parse(data.toString());
+    if ('TXid' in content) {
+      if (controllo_nonce(content.TXid.nonce, "qua ci va account nonce") && controllo_hash(content)) {
+        console.log(content);
+        console.log("transazione corretta");
+      } // qua va il nonce dell'account
+      else throw Error("nonce non valido");
+    } else {        
+      console.log(`client collegati : ${clients.length}`);
+      clients.forEach((element, i) => {
+        console.log(`client ${element.IP} connesso`);
+        // element.data.write(`Messaggio da ${socket.remoteAddress}: ${data}`);
+      });
+      socket.write(`Benvenuto ${socket.remoteAddress}`);
+    }
+  });
+  // client disconnessione
+  socket.on("end", function () {
+    clients = clients.filter((element) => element.IP != socket.remoteAddress);
+    let new_blocklist = new net.BlockList();
+    clients.forEach((element) => {
+      new_blocklist.addAddress(element);
+    });
+    blockList = new_blocklist; // blocklist con solo address che hanno il permesso
+    console.log(`client: ${socket.remoteAddress} scollegato\nclients collegati ${clients.length}`);
+  });
+  // errori
+  socket.on("error", (err) => {
+    console.error(`Socket error: ${err.message}`);
+  });
+});
+// porta 3000 indirizzi di ascolto tutti 0.0.0.0
+server.listen(3000, "0.0.0.0", () => {
+  console.log(`server dati: ${JSON.stringify(server.address())}`);
+});
+
+process.on("SIGINT", () => {
+  console.log("server chiuso");
+  clients.forEach((client, i) => {
+    // chiudi connessione con client
+    client.data.destroy();
+    clients.splice(i, 1);
+  });
+
+  if (clients.length === 0) process.exit(0);
+});
+
+function controllo_nonce(nonce_transection, nonce_account) {
+  return nonce_transection === 1;
+}
+
+function controllo_hash(data) {
+  const data_hash = `${data.amount},${data.sender},${data.reciver},${data.sender},${data.timestamp}`;
+  const hash = crypto.createHash("sha256").update(data_hash, "utf-8").digest("hex");
+  return hash === data.TXid.hash;
+}
---
+++ peers/client.js
@@ -0,0 +1,81 @@
+const net = require("net"); // protocollo TCP/IP
+const dgram = require("dgram"); // UDP è un protocollo di rete che consente l'invio di pacchetti di dati tra host in una rete senza stabilire una connessione formale
+const listen_server = new dgram.createSocket("udp4");
+const { Buffer } = require("buffer");
+const dns = require("dns");
+const os = require("os");
+const options = { family: 4 };
+// const { transection_id } = require("../blockChain");
+// console.log(transection_id);
+
+const port = 41234;
+const address = "255.255.255.255";
+let ip_address = "";
+let list_ip_address = new Set(); //vedere come mai mi dice connessione quando metto dentro '127.0.0.1' nel set anche se dopo nel server si connette solo uno
+
+listen_server.on("listening", () => {
+  // ottieni ip macchina
+  dns.lookup(os.hostname(), options, (err, addr) => {
+    if (err) {
+      console.error(err);
+    } else {
+      listen_server.setBroadcast(true);
+      // console.log(`IPv4 address: ${addr}`);
+      listen_server.send(Buffer.from(addr), port, address);
+    }
+  });
+});
+
+listen_server.on("error", (err) => {
+  console.error(err.message);
+});
+
+listen_server.on("message", (msg, rinfo) => {
+  // TODO PROVARE METODO MIGLIORE INVIANDO ARRAY E NON STRINGA
+  if (!ip_address.includes(rinfo.address)) {
+    ip_address += rinfo.address + ",";
+    list_ip_address.add(rinfo.address);
+    server_peer(list_ip_address);
+    console.log(list_ip_address);
+    listen_server.send(Buffer.from(ip_address), port, address);
+  }
+});
+listen_server.bind(port);
+
+// COLLEGAMENTO CLIENT
+function server_peer(IP_address) {
+  IP_address.forEach((IP_element) => {
+    console.log(IP_element);
+    const client = new net.Socket();
+
+    client.connect(3000, IP_element, () => {
+      console.log("client connesso");
+      const peer = { peer : IP_element };
+      client.write(JSON.stringify(peer));
+    });
+    // data ottenuti
+    client.on("data", (data) => {
+      const text = data.toString("utf-8");
+      console.log(text);
+    });
+    // errori
+    client.on("error", (err) => {
+      console.error(err.message);
+    });
+    // chiusura server
+    client.on("end", () => {
+      console.log("CONNESSIONE CHIUSA");
+      process.exit(0);
+    });
+    // chiusura client forzata
+    process.on("SIGINT", () => {
+      console.log("connessione chiusa");
+      client.end(() => {
+        // dato tempo a client di chiudere la connessione
+        setTimeout(() => {
+          process.exit(0);
+        }, 100);
+      });
+    });
+  });
+}
